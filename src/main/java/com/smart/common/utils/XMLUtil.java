cbfc1916e6554a3f448619fc39f0288ad42e709d
罗夏绝不妥协 <kobeliuziyang@gmail.com>
2018-09-09 21:20:53 +0800

    Update XMLUtil.java

--- src/main/java/com/smart/common/utils/XMLUtil.java
+++ src/main/java/com/smart/common/utils/XMLUtil.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by licheng on 2017/8/21.
+ * Created by lzy on 2017/8/21.
  */
 public class XMLUtil<T> {
     private Document doc;
